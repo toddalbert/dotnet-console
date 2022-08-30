da98ae100694fb9d639b05b3be0f8becafb07d3a
Todd Albert <todd@bocacode.com>
2022-08-30 11:08:05 -0400

    create account

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+BankAccount account = new BankAccount(47M);
+
+Console.WriteLine($"The balance of account is {account.Balance}");
